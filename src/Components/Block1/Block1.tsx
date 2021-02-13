5c39b316943b4513a27cb8933a2330dde6ab79b3
VilenaArturovna <vilena-forever@inbox.ru>
2021-02-13 14:40:27 +0600

    added particles

--- src/Components/Block1/Block1.tsx
+++ src/Components/Block1/Block1.tsx
@@ -1,10 +1,14 @@
+import React from 'react'
 import styleContainer from './../../Common/Styles/Container.module.css'
 import style from './Block1.module.css'
+import Particles from "react-particles-js";
+
 
 export const Block1 = () => {
     return (
         <div className={style.block}>
             <div className={styleContainer.container}>
+
                 <div className={style.about}>
                     IT Doc расширяет горизонты рынка ИТ-услуг нашего города и предлагает заключить договор на
                     обслуживание Вашей офисной печатной техники с простой и понятной структурой стоимости оказываемых
--- src/Components/Intro/Intro.tsx
+++ src/Components/Intro/Intro.tsx
@@ -4,11 +4,31 @@ import logotype from './../../Common/Images/logo.png'
 import {Button} from "@material-ui/core";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCloudDownloadAlt, faMobileAlt, faEnvelopeOpen} from "@fortawesome/free-solid-svg-icons";
+import Particles from "react-particles-js";
 
 export const Intro = () => {
     return (
         <section className={style.intro}>
             <div className={style.logotype}>
+                <Particles className={style.particles}
+                    params={{
+                        "particles": {
+                            "number": {
+                                "value": 250
+                            },
+                            "size": {
+                                "value": 3
+                            }
+                        },
+                        "interactivity": {
+                            "events": {
+                                "onhover": {
+                                    "enable": true,
+                                    "mode": "repulse"
+                                }
+                            }
+                        }
+                    }} />
                 <img src={logotype} className={style.logoImage} alt=""/>
                 <Button variant={"contained"} color={"secondary"} className={style.btn}>Перейти в каталог услуг</Button>
                 <Button variant={"contained"} color={"secondary"} className={style.btn}>Статус заказа</Button>
