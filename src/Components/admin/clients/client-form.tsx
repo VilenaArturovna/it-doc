9fd82c183acf8f75848af955f9d44b90e8afe12d
VilenaArturovna <vilena-forever@inbox.ru>
2024-05-27 19:06:40 +0600

    feat: added required flag to client form

--- src/Components/admin/clients/client-form.tsx
+++ src/Components/admin/clients/client-form.tsx
@@ -1,7 +1,7 @@
 import Title from 'antd/lib/typography/Title';
 import { Form, FormInstance, Input, Select } from 'antd';
 import { BackButton, SubmitButton } from '../../../ui';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import {
   Beneficiary,
@@ -31,7 +31,15 @@ export const ClientForm = ({
   formTitle,
   isCreating,
 }: Props) => {
-  useEffect(() => {}, [initialValues]);
+  const [required, setRequired] = useState(false);
+  const onClickSelect = () => {
+    if (form.getFieldsValue().type === ClientType.LegalPerson) {
+      setRequired(true);
+    } else {
+      setRequired(false);
+    }
+  };
+  useEffect(() => {}, [initialValues, required]);
   return (
     <Root>
       {contextHolder}
@@ -68,59 +76,55 @@ export const ClientForm = ({
         >
           <Input addonBefore="+7" />
         </Form.Item>
-        <Form.Item name={'fullName'} label={'Полное наименование'}>
+        <Form.Item name={'fullName'} label={'Полное наименование'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'legalAddress'} label={'Юридический адрес'}>
+        <Form.Item name={'legalAddress'} label={'Юридический адрес'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'actualAddress'} label={'Фактический адрес'}>
+        <Form.Item name={'actualAddress'} label={'Фактический адрес'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item label="Выгодоприобретатель" name="beneficiary">
+        <Form.Item label="Выгодоприобретатель" name="beneficiary" required={required}>
           <Select>
             <Select.Option value={Beneficiary.Ip}>ИП</Select.Option>
             <Select.Option value={Beneficiary.Ooo}>ООО</Select.Option>
           </Select>
         </Form.Item>
-        {isCreating ? (
-          <Form.Item label="Вид (ЮЛ/ФЛ)" name="type">
-            <Select>
-              <Select.Option value={ClientType.LegalPerson}>юридическое лицо</Select.Option>
-              <Select.Option value={ClientType.PhysicalPerson}>физическое лицо</Select.Option>
-            </Select>
-          </Form.Item>
-        ) : (
-          ''
-        )}
-        <Form.Item name={'INN'} label={'ИНН'}>
+        <Form.Item label="Вид (ЮЛ/ФЛ)" name="type" hidden={!isCreating}>
+          <Select onClick={onClickSelect}>
+            <Select.Option value={ClientType.LegalPerson}>юридическое лицо</Select.Option>
+            <Select.Option value={ClientType.PhysicalPerson}>физическое лицо</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name={'INN'} label={'ИНН'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'KPP'} label={'КПП'}>
+        <Form.Item name={'KPP'} label={'КПП'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'OGRN'} label={'ОГРН'}>
+        <Form.Item name={'OGRN'} label={'ОГРН'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'BIK'} label={'БИК'}>
+        <Form.Item name={'BIK'} label={'БИК'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'paymentAccount'} label={'Платежный счет'}>
+        <Form.Item name={'paymentAccount'} label={'Платежный счет'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'correspondentAccount'} label={'Корреспондентский счет'}>
+        <Form.Item name={'correspondentAccount'} label={'Корреспондентский счет'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'directorName'} label={'Директор'}>
+        <Form.Item name={'directorName'} label={'Директор'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'email'} label={'Адрес электронной почты'}>
+        <Form.Item name={'email'} label={'Адрес электронной почты'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'contactPerson'} label={'Контактное лицо'}>
+        <Form.Item name={'contactPerson'} label={'Контактное лицо'} required={required}>
           <Input />
         </Form.Item>
-        <Form.Item name={'contactPersonPhone'} label={'Телефон контактного лица'}>
+        <Form.Item name={'contactPersonPhone'} label={'Телефон контактного лица'} required={required}>
           <Input />
         </Form.Item>
         <ButtonGroup>
