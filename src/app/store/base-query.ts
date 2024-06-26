fae955ee02975e8865176c507db2daade2106369
VilenaArturovna <vilena-forever@inbox.ru>
2024-06-26 11:25:00 +0600

    feat: added ngrok support

--- src/app/store/base-query.ts
+++ src/app/store/base-query.ts
@@ -2,14 +2,20 @@ import SecureStorageService, { SecureStorageKeys } from '../../shared/services/s
 import { ENV } from '../../shared/config';
 import { fetchBaseQuery, retry } from '@reduxjs/toolkit/query';
 
+const baseUrl = ENV.apiUrl;
+
 export const baseQuery = retry(
   fetchBaseQuery({
-    baseUrl: ENV.apiUrl,
+    baseUrl,
     prepareHeaders: (headers) => {
       const token = SecureStorageService.getItem(SecureStorageKeys.TOKEN);
 
       if (token) {
         headers.set('authorization', `Bearer ${token}`);
+      }
+
+      if (baseUrl?.includes('ngrok')) {
+        headers.set('ngrok-skip-browser-warning', '69420');
       }
 
       return headers;
